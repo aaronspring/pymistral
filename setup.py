d3b13ae35d47120f64199194dc7af9c88fecc47d
Aaron Spring <aaron.spring@gmx.de>
2019-02-12 15:05:57 +0100

    bf

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ setup(name='pymistral',
       version='0.1',
       description='Wrapper for parallel, effective and efficient computations via slurm and dask on our supercomputer',
       url='http://github.com/aaronspring/pymistral',
-      authors=['Sebastian Milinski','Aaron Spring'],
-      author_email=['sebastian.milinski@mpimet.mpg.de','aaron.spring@mpimet.mpg.de'],
+      author=['Sebastian Milinski', 'Aaron Spring'],
+      author_email=['sebastian.milinski@mpimet.mpg.de',
+                    'aaron.spring@mpimet.mpg.de'],
       license='MIT',
       packages=['pymistral'],
       zip_safe=False,
       test_require=['pytest']
-     )
+      )
