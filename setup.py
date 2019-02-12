a66d4a38d9c1de76f824ec00ccc46a8b8b5fe65d
Aaron Spring <aaron.spring@gmx.de>
2019-02-12 16:12:03 +0100

    reproduce xskillscore

--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
-from setuptools import setup
+from setuptools import find_packages, setup
+
+test_requirements = ["pytest"]
+# with open("test_requirements-py36.txt") as f:
+#    install_requires = f.read().strip().split("\n")
+install_requires = ['xarray', 'dask']
+
+AUTHOR = ['Sebastian Milinski', 'Aaron Spring']
+AUTHOR_EMAIL = ['sebastian.milinski@mpimet.mpg.de',
+                'aaron.spring@mpimet.mpg.de']
 
 setup(name='pymistral',
       version='0.1',
-      description='Wrapper for parallel, effective and efficient computations via slurm and dask on our supercomputer',
+      description=[
+          'Wrapper for parallel, effective and efficient computations via slurm and dask on our supercomputer'],
       url='http://github.com/aaronspring/pymistral',
-      authors=['Sebastian Milinski','Aaron Spring'],
-      author_email=['sebastian.milinski@mpimet.mpg.de','aaron.spring@mpimet.mpg.de'],
+      author=AUTHOR,
+      author_email=AUTHOR_EMAIL,
       license='MIT',
-      packages=['pymistral'],
+      packages=find_packages(),
       zip_safe=False,
-      test_require=['pytest']
-     )
+      install_requires=install_requires,
+      test_suite="tests",
+      tests_require=test_requirements,
+      python_requires=['>=3.6']
+      )
