05975eb993049bab93a03434f2054ce5ca85d27e
Aaron Spring <aaron.spring@gmx.de>
2019-02-12 13:36:45 +0100

    add find packages

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(name='pymistral',
       version='0.1',
       description='Wrapper for parallel, effective and efficient computations via slurm and dask on our supercomputer',
       url='http://github.com/aaronspring/pymistral',
-      authors=['Sebastian Milinski','Aaron Spring'],
-      author_email=['sebastian.milinski@mpimet.mpg.de','aaron.spring@mpimet.mpg.de'],
+      authors=['Sebastian Milinski', 'Aaron Spring'],
+      author_email=['sebastian.milinski@mpimet.mpg.de',
+                    'aaron.spring@mpimet.mpg.de'],
       license='MIT',
-      packages=['pymistral'],
+      packages=find_packages(),
       zip_safe=False,
       test_require=['pytest']
-     )
+      )
