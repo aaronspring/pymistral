67ea0bcbf390ec4937dae296cca26b8c86cb82a5
Aaron Spring <aaron.spring@gmx.de>
2019-02-15 10:15:40 +0100

    antoher try

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import find_packages, setup
 
-test_requirements = ["pytest"]
+TEST_REQUIRES = ["pytest"]
 
-install_requires = ['bokeh',
+INSTALL_REQUIRES = ['bokeh',
+                    'netcdf4'
                     'cartopy',
                     'dask',
                     'dask-jobqueue',
@@ -20,18 +21,19 @@ install_requires = ['bokeh',
 AUTHOR = ['Sebastian Milinski', 'Aaron Spring']
 AUTHOR_EMAIL = ['sebastian.milinski@mpimet.mpg.de',
                 'aaron.spring@mpimet.mpg.de']
-
+DESCRIPTION = [
+    'Wrapper for parallel, effective and efficient computations via slurm and dask on our supercomputer']
 setup(name='pymistral',
       version='0.1',
-      description=[
-          'Wrapper for parallel, effective and efficient computations via slurm and dask on our supercomputer'],
+      description=DESCRIPTION,
       url='http://github.com/aaronspring/pymistral',
       author=AUTHOR,
       author_email=AUTHOR_EMAIL,
       license='MIT',
       packages=find_packages(),
       zip_safe=False,
-      install_requires=install_requires,
-      test_suite="tests",
-      tests_require=test_requirements
+      install_requires=INSTALL_REQUIRES,
+      tests_require=TEST_REQUIRES,
+      py_modules=["cdo"],
+      requires_python=['>=3.6']
       )
