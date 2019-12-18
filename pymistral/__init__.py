f3d5d6ee07ed2ebde56a133c74ee082852c3e343
AS <aaron.spring@mpimet.mpg.de>
2019-12-18 21:57:22 +0100

    rm testing bc in esmtools

--- pymistral/__init__.py
+++ pymistral/__init__.py
@@ -10,9 +10,8 @@ Available Modules
 - plot: Cartopy Mapping for MPIOM curvilinear grids
 - hamocc: HAMOCC-specific helper functions
 - slurm_post: write python code to file and send to SLURM (experimental)
-- testing: FDR for xarray masked
 - cdo_post: postprocessing with CDO into xarray
 """
 
-from . import cdo_post, hamocc, plot, setup, slurm_post, testing
+from . import cdo_post, hamocc, plot, setup, slurm_post
 from .setup import cdo
